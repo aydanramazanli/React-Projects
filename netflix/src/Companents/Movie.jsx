6c12485370655415df13649f0d7abb67cc4aaa79
Aydan <aydan2903@gmail.com>
2021-12-20 00:00:46 +0400

    movies slider

--- netflix/src/Companents/Movie.jsx
+++ netflix/src/Companents/Movie.jsx
@@ -3,38 +3,39 @@ import { Link } from "react-router-dom";
 import "..//Css/MovieList.css";
 
 const style={
-  width:"1100px",
-
+  width:"832px",
 }
 
 export default function Movie({ category, movies }) {
 const [value, setValue]= useState(0)
-let dataLength=movies.Length
-console.log(typeof movies)
+let dataLength=movies.length
+
+
 
 const nextSlider=()=>{
-  if(value !== -(dataLength-1) * style.width){
-    setValue(value-style.width)
+  if(value > -((dataLength-(dataLength%4)) * 208)){
+    setValue(value-parseFloat(style.width.replace(/\D/g,'')))
   }
 }
-
+  
 const prevSlider=()=>{
-  if(value!==0){
-    setValue(value+style.width)
+  if(value+parseFloat(style.width.replace(/\D/g,'')) <= 0){
+    setValue(value+parseFloat(style.width.replace(/\D/g,'')))
   }
 }
 
   return (
     <div className="movies-section">
+    <div className=" box-border	overflow-hidden">
       <div className="py-2">
         <h2 className="text-gray-50 text-2xl font-bold my-3">{category}</h2>
       </div>
-
-    
       <div
-    
-      style={style}
-        className="movies inline-flex flex-nowrap w-full relative "
+      style={{
+        left: `${value}px`,
+      }}
+
+        className="movies inline-flex flex-nowrap relative transition-all	"
       >
       
         {movies?.map((info) => {
@@ -42,7 +43,7 @@ const prevSlider=()=>{
 
             <Link
               to={`film-detail/${info.id}`}
-              className="movie-link  w-52 gap-5"
+              className="movie-link w-48 h-48 gap-5 mx-2"
               target="_blank"
             >
               <div className="movie-item h-full">
@@ -60,11 +61,14 @@ const prevSlider=()=>{
 
        
       </div>
-     <div className="buttons">
-       <button className="next" onClick={nextSlider}>Next</button>
-       <button className="prev" onClick={prevSlider}>Prev</button>
-     </div>
+
  
     </div>
+    <div className="buttons">
+       <button className="next" onClick={nextSlider}><i class="fas fa-chevron-right text-gray-50"></i></button>
+       <button className="prev" onClick={prevSlider}><i class="fas fa-chevron-left text-gray-50"></i></button>
+     </div>
+    </div>
+
   );
 }
